f9dd2efef2a900da9c7dba124fc962dc05e10e63
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-09 18:28:27 +0300

    added SearchDelegate

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Searchbox'),
+      home: const MyHomePage(title: 'Search'),
     );
   }
 }
@@ -28,16 +28,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          IconButton(
+              onPressed: () {
+                showSearch(context: context, delegate: MySearchDelegate());
+              },
+              icon: const Icon(Icons.search))
+        ],
       ),
-      body:SizedBox(),
+      body: SizedBox(),
     );
+  }
+}
+
+class MySearchDelegate extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+        IconButton(
+            onPressed: () {
+              if(query.isEmpty){
+                close(context, null);
+              }else{
+                query = "";
+              }
+            },
+            icon: const Icon(Icons.clear))
+      ];
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+      onPressed: () => close(context, null),
+      icon: const Icon(Icons.arrow_back));
+
+  @override
+  Widget buildResults(BuildContext context) => Container();
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions=[
+      "Uşak",
+      "Denizli",
+      "Sakarya",
+      "İstanbul",
+      "Londra"
+    ];
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context,index){
+      final suggestion=suggestions[index];
+      return ListTile(
+        title: Text(suggestion),
+        onTap: (){
+
+        },
+      );
+    },);
   }
 }
